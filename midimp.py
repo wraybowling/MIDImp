f9cf16913d26020c505c7195c3d8437ba5c7dbe1
Wray Bowling <1120619+wraybowling@users.noreply.github.com>
2022-05-03 23:06:59 -0400

    Fixes #1 Adds --device argument

--- midimp.py
+++ midimp.py
@@ -8,61 +8,45 @@ from tkinter.tix import INTEGER
 from tokenize import Number
 ### osc imports
 import argparse
-# import random
-# import time
 from pythonosc import udp_client
 from pythonosc import osc_message_builder
 
-"""
-List available ports.
-This is a simple version of mido-ports.
-"""
-def print_ports(heading, port_names):
-    print(heading)
-    for i in range(len(port_names)):
-        print("    {}:'{}'".format(i,port_names[i]))
-    print()
 
-input_port_options = mido.get_input_names()
-
-print_ports('Input Ports:', input_port_options)
-print_ports('Output Ports:', mido.get_output_names())
-
-val = int(input('Choose input: '))
-portname = input_port_options[val]
-print(portname)
-
-
-"""Small example OSC client
-
-This program sends 10 random values between 0.0 and 1.0 to the /filter address,
-waiting for 1 seconds between each value.
-"""
-
-ARG_TYPE_MIDI = "m";
-
-# if __name__ == "__main__":
+# Commandline Arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--ip", default="192.168.1.145",
+parser.add_argument("--ip", default="127.0.0.1",
     help="The ip of the OSC server")
 parser.add_argument("--port", type=int, default=9000,
     help="The port the OSC server is listening on")
+parser.add_argument('--device', type=int,
+    help="The MIDI input device to send")
 args = parser.parse_args()
 
+
+# Choose MIDI input
+input_port_options = mido.get_input_names()
+
+if args.device is None:
+    def print_ports(heading, port_names):
+        print(heading)
+        for i in range(len(port_names)):
+            print("    {}:'{}'".format(i,port_names[i]))
+        print()
+
+    print_ports('Input Ports:', input_port_options)
+    print_ports('Output Ports:', mido.get_output_names())
+
+    portname = input_port_options[int(input('Choose input: '))]
+else:
+    portname = input_port_options[args.device]
+
+
+# OSC client
 client = udp_client.SimpleUDPClient(args.ip, args.port)
 
-#   for x in range(10):
-#     client.send_message("/notes", random.random())
-#     time.sleep(1)
 
-
-"""
-Receive messages from the input port and print them out.
-"""
-# if len(sys.argv) > 1:
-#     portname = sys.argv[1]
-# else:
-#     portname = None  # Use default port
+# Receive messages from MIDI and send them
+ARG_TYPE_MIDI = "m"
 
 try:
     with mido.open_input(portname) as port:
@@ -70,7 +54,6 @@ try:
         print('Waiting for messages...')
         for message in port:
             print('Received {} {}'.format(message, message.bytes()))
-            # client.send_message("/notes", message)
             midi_bytes = message.bytes()
             builder = osc_message_builder.OscMessageBuilder(address="/notes")
             builder.add_arg((1, midi_bytes[0], midi_bytes[1], midi_bytes[2]), builder.ARG_TYPE_MIDI)
